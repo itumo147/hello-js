4cdcbe9f0f9df2139bba6dffd0bee3087ed6014a
martyns <itumo147@yahoo.com>
2024-02-27 11:18:31 +0100

    micro-finance

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+console.log(">>>>>>welcome to gmc micro-finance bank<<<<<<");
+
+console.log("create a new account");
+var customerId=prompt("enter your name");
+var customerId=prompt("enter your phone number");
+var customerId=prompt("enter your email address");
+var customerId=prompt("enter your bvn");
+var customerId=prompt("enter pin");
+var customerId=prompt("re-enter pin");
+console.log("check balance");
+console.log("deposite");
+console.log("withdraw");
+console.log("transfer");
+console.log("quit");
+console.log(customerId + "martyns. your account has been created successfully");
